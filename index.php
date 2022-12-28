360e6dc3dd16b592a0b77fefab44d5ae1ee656c2
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-12-28 18:33:41 +0100

    setup folder tree and basic files

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    require_once 'bootstrap.php';
+
+    $templateParams["title"] = "bevero";
+
+    require_once 'template/base.php';
+?>
---
+++ template/base.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $templateParams["title"]; ?></title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+<body>
+    <!--TODO-->
+</body>
+</html>
