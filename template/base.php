10a1da8fa3bfc770f851e0ff911a43285de656bc
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-12-28 19:29:05 +0100

    add navbar to base template

--- template/base.php
+++ template/base.php
@@ -6,6 +6,21 @@
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
 </head>
 <body>
-    <!--TODO-->
+    <nav>
+        <ul>
+        <li>
+            <a <?php isActive("index.php");?> href="index.php"><img ></a>
+        </li>
+        <li>
+            <a <?php isActive("search.php");?> href="search.php"><img ></a>
+        </li>
+        <li>
+            <a <?php isActive("notification.php");?> href="notification.php"><img ></a>
+        </li>
+        <li>
+            <a <?php isActive("profile.php");?> href="profile.php"><img ></a>
+        </li>
+        </ul>
+    </nav>
 </body>
 </html>
