632645d3c2d9512db9aa9a84da7c48b3492dd7c3
Luca Lucioli <luca.lucioli@studio.unibo.it>
2023-05-20 18:13:21 +0200

    make feed-nav sticky

--- template/feed.php
+++ template/feed.php
@@ -1,17 +1,16 @@
-        <nav aria-label="feed-menu">
-            <ul>
-                <li>
-                    <a href="index.php" class="active" onclick="switchHome(true);return false;" id="followers">followers</a>
-                </li>
-                <li>
-                    <a href="index.php" onclick="switchHome(false);return false;" id="discovery">discovery</a>
-                </li>
-            </ul>
-        </nav>
-
         <div id="scrollable_div">
+            <nav aria-label="feed-menu">
+                <ul>
+                    <li>
+                        <a href="index.php" class="active" onclick="switchHome(true);return false;" id="followers">followers</a>
+                    </li>
+                    <li>
+                        <a href="index.php" onclick="switchHome(false);return false;" id="discovery">discovery</a>
+                    </li>
+                </ul>
+            </nav>
             <section></section>
-        
+
             <footer>
                 <button onclick="loadMore();return false;">load more</button>
             </footer>
