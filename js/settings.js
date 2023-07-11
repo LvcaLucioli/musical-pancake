bf2e0d26e9a139aafba3df7cef0b7e07be93193b
Luca Lucioli <luca.lucioli@studio.unibo.it>
2023-07-11 03:37:20 +0200

    logout function in settings page

--- js/settings.js
+++ js/settings.js
@@ -42,3 +42,20 @@ document.querySelector('.login form').addEventListener('submit', function (event
             }
         })
 })
+
+function logout() {
+    fetch('api/api-logout.php', {
+        method: 'POST',
+        credentials: 'same-origin' 
+    })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = 'login.php';
+            } else {
+                console.error('error during logout');
+            }
+        })
+        .catch(error => {
+            console.error('error in logout request:', error);
+        });
+}
